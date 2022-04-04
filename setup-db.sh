2d3f333f4c3189556c38f6297ddf26d4496def03
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-04 08:06:35 +0530

    Update kubernetes/

--- setup-db.sh
+++ setup-db.sh
@@ -12,16 +12,9 @@ helm upgrade -i mongodb bitnami/mongodb --set auth.enabled=false
 
 kubectl run debug --image=centos:7 -- sleep 100000
 
-kubectl exec debug -- yum install epel-release -y
-kubectl exec debug -- curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-kubectl exec debug -- yum install mongodb-org mariabd unzip -y
-kubectl exec debug -- curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-kubectl exec debug -- curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-kubectl exec debug -- unzip -o /tmp/mongodb.zip
-kubectl exec debug -- unzip -o /tmp/mysql.zip
-kubectl exec debug -- "mongo --host mongodb </mongodb-main/catalogue.js"
-kubectl exec debug -- mongo --host mongodb \</mongodb-main/users.js
-kubectl exec debug -- mysql -h mysql -uroot -ppassword \</mysql-main/shipping.sql
+kubectl cp debug db-load.sh debug:/db-load.sh 
+kubectl exec debug -- /db-load.sh
+
 
 kubectl exec rabbitmq-0 -- rabbitmqctl add_user roboshop roboshop123
 kubectl exec rabbitmq-0 -- rabbitmqctl set_user_tags roboshop administrator
