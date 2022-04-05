579261c8182765a75afe833ff350853bdedd3541
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-05 08:55:55 +0530

    Update kubernetes/

--- setup-db.sh
+++ setup-db.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-NAMESPACE=default
+NAMESPACE=dev
 
-helm repo add stable https://charts.helm.sh/stable
-helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add stable https://charts.helm.sh/stable -n ${NAMESPACE}
+helm repo add bitnami https://charts.bitnami.com/bitnami -n ${NAMESPACE}
 
-helm upgrade -i mysql stable/mysql --set mysqlRootPassword=password
-helm upgrade -i rabbitmq bitnami/rabbitmq
-helm upgrade -i redis bitnami/redis --set auth.enabled=false
-helm upgrade -i mongodb bitnami/mongodb --set auth.enabled=false
+helm upgrade -i mysql stable/mysql --set mysqlRootPassword=password -n ${NAMESPACE}
+helm upgrade -i rabbitmq bitnami/rabbitmq -n ${NAMESPACE}
+helm upgrade -i redis bitnami/redis --set auth.enabled=false -n ${NAMESPACE}
+helm upgrade -i mongodb bitnami/mongodb --set auth.enabled=false -n ${NAMESPACE}
 
-kubectl run debug --image=centos:7 -- sleep 100000
+kubectl run debug -n ${NAMESPACE} --image=centos:7 -- sleep 100000
 
-kubectl cp db-load.sh debug:/db-load.sh
-kubectl exec debug -- bash /db-load.sh
+kubectl -n ${NAMESPACE} cp db-load.sh debug:/db-load.sh
+kubectl -n ${NAMESPACE} exec debug -- bash /db-load.sh
 
 
-kubectl exec rabbitmq-0 -- rabbitmqctl add_user roboshop roboshop123
-kubectl exec rabbitmq-0 -- rabbitmqctl set_user_tags roboshop administrator
-kubectl exec rabbitmq-0 -- rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+kubectl -n ${NAMESPACE} exec rabbitmq-0 -- rabbitmqctl add_user roboshop roboshop123
+kubectl -n ${NAMESPACE} exec rabbitmq-0 -- rabbitmqctl set_user_tags roboshop administrator
+kubectl -n ${NAMESPACE} exec rabbitmq-0 -- rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 
