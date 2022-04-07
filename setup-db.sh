7fb61263053de69630a57f267577663d58c87e60
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-07 07:54:38 +0530

    Update kubernetes/

--- setup-db.sh
+++ setup-db.sh
@@ -4,11 +4,14 @@ NAMESPACE=dev
 
 helm repo add stable https://charts.helm.sh/stable -n ${NAMESPACE}
 helm repo add bitnami https://charts.bitnami.com/bitnami -n ${NAMESPACE}
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
 
 helm upgrade -i mysql stable/mysql --set mysqlRootPassword=password -n ${NAMESPACE}
 helm upgrade -i rabbitmq bitnami/rabbitmq -n ${NAMESPACE}
 helm upgrade -i redis bitnami/redis --set auth.enabled=false -n ${NAMESPACE}
 helm upgrade -i mongodb bitnami/mongodb --set auth.enabled=false -n ${NAMESPACE}
+helm upgrade -i prometheus prometheus-community/kube-prometheus-stack
 
 kubectl run debug -n ${NAMESPACE} --image=centos:7 -- sleep 100000
 
